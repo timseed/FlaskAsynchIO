e00342a789f9932fad43a04135b10f4f4ea29d23
Tim Seed <tim@sy-edm.com>
2020-07-22 14:33:50 +0800

    Static and Dynamic

--- __main__.py
+++ __main__.py
@@ -4,5 +4,5 @@ from controllers.app import app
 
 if __name__ == '__main__':
     from model.rnd_num import *
-
+    from model.hello import *
     socketio.run(app, host='127.0.0.1', port=5000)
--- model/hello.py
+++ model/hello.py
@@ -3,6 +3,6 @@ from flask import Flask, render_template, url_for, copy_current_request_context
 
 
 @app.route('/hello')
-def index():
+def hello():
     #only by sending this page first will the client be connected to the socketio instance
     return render_template('Hello.html')
